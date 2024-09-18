cfa40d9bd07c5cb0dab6e479973b76bb34cbecc5
ILTD <heinhtet9512@gmail.com>
2024-09-18 22:32:41 +0900

    mobile view update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menuBtn = document.getElementById("menu-btn");
+const menuClose = document.getElementById("menu-close");
 const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-  menuBtn.classList.toggle("cross");
+  menu.classList.remove("hidden");
+  menuBtn.classList.add("hidden"); // Hide the burger menu button when menu opens
+});
+
+menuClose.addEventListener("click", () => {
+  menu.classList.add("hidden");
+  menuBtn.classList.remove("hidden"); // Show the burger menu button when menu closes
 });
