fef96b38e695448ef7c3335bab192e1856e41677
ILTD <heinhtet9512@gmail.com>
2024-10-13 19:23:21 +0900

    mostly finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
+  content: ["*.{html,js}", "./pages/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        iphonese: "375px", // iPhone SE
+        iphone12: "390px", // iPhone 12
+        iphone14: "430px",
+      },
+    },
   },
   plugins: [],
 };
